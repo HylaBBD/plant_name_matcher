073b3da2ab9aa6f905711c4917dd265f205e2ebd
Hyla Fourie <hyla@bbd.co.za>
2023-05-18 15:56:38 +0200

    more moreeeee

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 // PICTURE AND LATIN NAMES - - - - - - - - - - - - 
 // const key = "sk-CtSG645bb2c924947866"; // JESSE BBD EMAIL
-const key = "sk-q8UK6466165b60d98975"; // JESSE jess44go EMAIL
+// const key = "sk-q8UK6466165b60d98975"; // JESSE jess44go EMAIL
+const key = "sk-Ir4T64662d40d85b0978";
 const level = 1;
 let numberPlants;
 

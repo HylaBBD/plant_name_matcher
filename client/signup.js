7fd52e82a4a976f32445b1245c8dbc0878526640
Jesse van der Merwe <jesse@bbd.co.za>
2023-05-24 01:34:42 +0200

    Trying to add signup validation

--- client/signup.js
+++ client/signup.js
@@ -6,18 +6,28 @@ const userName = document.getElementById("user-name");
 const userPassword1 = document.getElementById("user-password1");
 const userPassword2 = document.getElementById("user-password2");
 
+function isValidPassword(password) { 
+  return !!(/.*[A-Za-z].*/.test(password));
+}
+
+
 async function signup(event) {
   event.preventDefault(); // Prevent form submission from reloading the page
   if(userName.value != "" && userPassword1.value != "" && userPassword2.value != ""){
     if(userPassword1.value == userPassword2.value){
-      try {
-        console.log(userName.value);
-        console.log(userPassword1.value);
-        const response = await userService.register(userName.value, userPassword1.value);
-        const data = await response.json();
-        errorText.innerText = "Successfully registered. Please login.";
-      } catch (error) {
-        errorText.innerText = "Invalid login details";
+      if(isValidPassword(userPassword1)){
+      // if(userPassword1 <=0 || userPassword1 >=0 ){
+        try {
+          console.log(userName.value);
+          console.log(userPassword1.value);
+          const response = await userService.register(userName.value, userPassword1.value);
+          const data = await response.json();
+          errorText.innerText = "Successfully registered. Please login.";
+        } catch (error) {
+          errorText.innerText = "Invalid login details";
+        }
+      }else{
+        errorText.innerText = "Please ensure your password contains letters.";
       }
     }else{
       errorText.innerText = "Please ensure the passwords are identical.";

827d9a93efbcc8330b8e1f7b0a509767f90734e7
Jesse van der Merwe <jesse@bbd.co.za>
2023-05-24 14:55:51 +0200

    Input validation to prevent specials chars or just numbers in the password or username

--- client/signup.js
+++ client/signup.js
@@ -7,35 +7,45 @@ const userPassword1 = document.getElementById("user-password1");
 const userPassword2 = document.getElementById("user-password2");
 
 function isValidPassword(password) {
-  return true;//!!(/^[a-zA-Z0-9]+$/.test(password) && !(/^[0-9]+$/.test(password)));
+  let specialChars =/[`!@#$%^&*()_\-+=\[\]{};':"\\|,.<>\/?~ ]/;
+  if(specialChars.test(password)){
+    return false;
+  }
+  return true;
 }
 
-
-async function signup(event) {
-  event.preventDefault(); // Prevent form submission from reloading the page
-  if(userName.value != "" && userPassword1.value != "" && userPassword2.value != ""){
-    if(userPassword1.value == userPassword2.value){
-      if(isValidPassword(userPassword1)){
-      // if(userPassword1 <=0 || userPassword1 >=0 ){
-        try {
-          console.log(userName.value);
-          console.log(userPassword1.value);
-          const response = await userService.register(userName.value, userPassword1.value);
-          const data = await response.json();
-          errorText.innerText = "Successfully registered. Please login.";
-        } catch (error) {
-          errorText.innerText = "Invalid login details";
-        }
-      }else{
-        errorText.innerText = "Please ensure your password contains letters.";
-      }
-    }else{
-      errorText.innerText = "Please ensure the passwords are identical.";
-    }
-  }else{
-    errorText.innerText = "Please fill in all fields.";
+function containsLetters(password) {
+  let letterChars = /[A-Za-z\s]/;
+  if(letterChars.test(password)){
+    return true;
   }
-
+  return false;
+}
+async function signup(event) {
+  errorText.innerText = "";
+  event.preventDefault(); // Prevent form submission from reloading the page
+  if(userName.value == "" || userPassword1.value == "" || userPassword2.value == ""){
+    errorText.innerText = "Please fill in all fields.";
+  }else if(!containsLetters(userName.value)){
+    errorText.innerText = "Username must contains some letters.";
+  }else if(!isValidPassword(userName.value)){
+    errorText.innerText = "Username can only contains letters and numbers.";
+  }else if(userPassword1.value != userPassword2.value){
+    errorText.innerText = "Please ensure the passwords are identical.";
+  }else if(!containsLetters(userPassword1.value)){
+    errorText.innerText = "Password must contains some letters.";
+  }else if(!isValidPassword(userPassword1.value)){
+    errorText.innerText = "Password can only contains letters and numbers.";
+  }
+  else{
+    try {
+      const response = await userService.register(userName.value, userPassword1.value);
+      const data = await response.json();
+      errorText.innerText = "Successfully registered. Please login.";
+    } catch (error) {
+      errorText.innerText = "Invalid login details.";
+    }
+  }
 }
 
 singupForm.addEventListener("submit", signup);

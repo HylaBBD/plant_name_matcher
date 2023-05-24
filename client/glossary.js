47f29d290b48581aed246f515e6a645329e1843f
Jesse van der Merwe <jesse@bbd.co.za>
2023-05-24 14:24:51 +0200

    more responsiveness + glossary number thingy

--- client/glossary.js
+++ client/glossary.js
@@ -1,7 +1,30 @@
 import { userService } from "./services/user.service.js";
 import { plantService } from "./services/plant.service.js";
+
+// NAV CONTROLLER
 const navControlIndex = document.getElementById("navigation-control-index");
 const navControlMax = document.getElementById("navigation-control-max");
+
+navControlIndex.addEventListener("change", (event) => plantIdValueChange(event));
+
+function plantIdValueChange(event){
+    let value = event.target.value;
+    console.log(value);
+
+    if(Number.isInteger(value)){
+        if(value <= 1){
+            value = 1;
+        }else if(value >= 150){
+            value = 150;
+        }
+    }else{
+        value = 1;
+    }
+    plantID = value-1;
+    navControlIndex.value = value;
+    displayLoadingScreen();
+    onSelectorClick(0).then(() => hideLoadingScreen());    
+}
 
 // LEFT AND RIGHT SELECTORS
 let leftSelector = document.getElementById("leftSelector");
@@ -12,6 +35,7 @@ let rightSelector = document.getElementById("rightSelector");
 if (rightSelector != null) {
     rightSelector.addEventListener("click", function () { onSelectorClick(1) });
 }
+
 
 // LOADING SCREEN - - - - - - - - - - - - 
 const loadingSection = document.getElementById("loading-screen");
@@ -30,7 +54,7 @@ function hideLoadingScreen() {
 let plants = await plantService.getPlants();
 navControlMax.innerText = plants.length;
 let plantID = 0;
-navControlIndex.innerText = plantID;
+navControlIndex.value = plantID+1;
 
 let userFavouritePlants = await userService.getUserFavouritePlant(localStorage.getItem("userId"));
 let userFavouritePlantIDs = userFavouritePlants.map((favouritePlant) => {return favouritePlant.id});
@@ -82,12 +106,17 @@ async function onSelectorClick(change){
     let scientificName = document.getElementById("scientific-name");
 
     plantID = plantID + change;
-    if(plantID < 0){
+    if(plantID <= 0){
         plantID = 0;
-    }else if(plantID > plants.length){
-        plantID = plants.length;
+        leftSelector.classList.add("disabled");
+    }else if(plantID >= 149){
+        plantID = 149;
+        rightSelector.classList.add("disabled");
+    }else{
+        leftSelector.classList.remove("disabled");
+        rightSelector.classList.remove("disabled");
     }
-    navControlIndex.innerText = plantID+1;
+    navControlIndex.value = plantID+1;
     commonName.textContent = plants[plantID].commonName;
     scientificName.textContent = plants[plantID].scientificName;
     glossaryImage.src = plants[plantID].defaultImage.original_url;
